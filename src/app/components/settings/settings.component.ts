7210909c9b052f2f46e00963f003edb61b934301
FenixAlive <luisangelmf@gmail.com>
2023-05-26 17:40:05 -0600

    miss logic settings

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirebaseService } from 'src/app/http/firebase.service';
 
@@ -8,8 +9,21 @@ import { FirebaseService } from 'src/app/http/firebase.service';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  isLoading = false;
+  settingsForm: FormGroup;
 
-  constructor(public readonly firebaseService: FirebaseService, private router: Router) { }
+  constructor(public readonly firebaseService: FirebaseService, private router: Router, private fb: FormBuilder) {
+    this.settingsForm = this.fb.group(
+      {
+        name: [this.firebaseService.auth.currentUser?.displayName],
+        email: [this.firebaseService.auth.currentUser?.email, [Validators.required, Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)]],
+        photo: [this.firebaseService.auth.currentUser?.photoURL],
+        phone: [this.firebaseService.auth.currentUser?.phoneNumber],
+        password: ['', [Validators.minLength(8)]],
+        rePassword: ['', [Validators.minLength(8)]],
+      }
+    )
+  }
 
   ngOnInit(): void {
     console.log(this.firebaseService.auth)
